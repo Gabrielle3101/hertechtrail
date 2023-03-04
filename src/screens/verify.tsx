931cebb64f0dfee2c71cb6ed5df9977f6f385f80
Gabrielle3101 <godd3101@gmail.com>
2023-03-04 22:29:41 +0100

    changed images

--- src/screens/verify.tsx
+++ src/screens/verify.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import Button from '../components/button'
 import { Link } from 'react-router-dom'
-import Msg from "../assets/msg.png"
-import TextInput from '../components/textinput'
 
 const Verify = () => {
   const [value, setValue] = React.useState('');
@@ -10,7 +8,11 @@ const Verify = () => {
     <div className='w-full h-screen flex flex-col  px-6 pt-12'>
       <h3 className=' font-bold text-2xl mb-5'>Verify Your Identity</h3>
       <h5 className=' font-semibold text-1xl mb-3'>Enter the 6-digit OTP sent to 0803xxxxx94</h5>
-      <Msg />
+      <div className='w-32 h-32 rounded-full bg-green-600 mb-10 p-4'>
+        <div className='w-full h-full rounded-full bg-white p-4'>
+        <div className='w-full h-full rounded-full bg-green-400 p-4'></div>
+        </div>
+      </div>
       <h5 className=' font-semibold text-green text-1xl mb-3'>_ _ _ _ _ _</h5>
       <div className='flex'>
             <h5 className=' font-semibold text-1xl mb-3'>Didn’t receive an OTP? &nbsp; </h5>
