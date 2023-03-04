022d28c4ff1d3ed9f614a6134f738c8f23e580dd
Gabrielle3101 <godd3101@gmail.com>
2023-03-04 22:19:21 +0100

    verification

--- src/screens/verify.tsx
+++ src/screens/verify.tsx
@@ -9,15 +9,15 @@ const Verify = () => {
     <div className='w-full h-screen flex flex-col  px-6 pt-12'>
       <h3 className=' font-bold text-2xl mb-5'>Verify Your Identity</h3>
       <h5 className=' font-semibold text-1xl mb-3'>Enter the 6-digit OTP sent to 0803xxxxx94</h5>
-      <img src="./assets/msg.png" alt="" />
+      <img src="/msg.png" alt="" />
       <h5 className=' font-semibold text-green text-1xl mb-3'>_ _ _ _ _ _</h5>
       <div className='flex'>
-            <h5 className=' font-semibold text-1xl mb-3'>Didn’t receive an OTP?</h5>
-            <a href="">Resend</a>
+            <h5 className=' font-semibold text-1xl mb-3'>Didn’t receive an OTP? &nbsp; </h5>
+            <a className='text-green' href="">Resend</a>
       </div>
       <div className='flex'>
-            <h5 className=' font-semibold text-1xl mb-3'>Don’t have access to 0803xxxxx94?</h5>
-            <a href="">Click Here</a>
+            <h5 className=' font-semibold text-1xl mb-3'>Don’t have access to 0803xxxxx94? &nbsp;</h5>
+            <a className='text-green' href="">Click Here</a>
       </div>
       <Link to={`/verify`} className='w-full'>
       <Button label="Proceed" handleClick={() => {}} />
