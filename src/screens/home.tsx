d99681f862ca7338d33bb1b1e9f6ccf11f05ea87
Gabrielle3101 <godd3101@gmail.com>
2023-03-04 21:33:47 +0100

    first commit

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -8,7 +8,7 @@ const Home = () => {
   return (
     <div className='w-full h-screen flex flex-col  px-6 pt-12'>
       <h3 className=' font-bold text-2xl mb-5'>Register</h3>
-      <h5 className=' font-semibold text-2xl mb-5'>Choose an ID and enter number</h5>
+      <h5 className=' font-semibold text-1xl mb-3'>Choose an ID and enter number</h5>
       <TextInput value={value} handleChange={(e) => setValue(e)} placeholder='Choose ID type' />
       <TextInput value={value} handleChange={(e) => setValue(e)} placeholder='Enter ID No.' />
       <Link to={`/auth`} className='w-full'>
