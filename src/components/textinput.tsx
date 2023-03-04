5e894e26045c1198e43114180220894c8aab34f6
Gabrielle3101 <godd3101@gmail.com>
2023-03-05 00:09:30 +0100

    fixed errors

--- src/components/textinput.tsx
+++ src/components/textinput.tsx
@@ -8,7 +8,7 @@ interface IProps {
 
 const TextInput = ({ value, placeholder = 'Enter text', handleChange }: IProps) => {
   return (
-    <input onChange={(e) => handleChange(e.target.value)} value={value} placeholder={placeholder} className='w-full break-normal min-h-12 rounded-md border-1 border-gray-200 px-2 mb-4 bg-gray-100' />
+    <input onChange={(e) => handleChange(e.target.value)} value={value} placeholder={placeholder} className='w-full break-normal h-12 rounded-md border-1 border-gray-200 px-2 mb-4 bg-gray-100' />
   )
 }
 
