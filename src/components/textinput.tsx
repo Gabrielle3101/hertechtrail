4bcf63054cf7e5ddebc6d8413ef855a44ab621e1
Gabrielle3101 <godd3101@gmail.com>
2023-03-06 17:31:35 +0100

    required

--- src/components/textinput.tsx
+++ src/components/textinput.tsx
@@ -2,13 +2,14 @@ import React from 'react'
 
 interface IProps {
     value: string;
+    required: true;
     placeholder?: string;
     handleChange: (e: string) => void;
 }
 
-const TextInput = ({ value, placeholder = 'Enter text', handleChange }: IProps) => {
+const TextInput = ({ value, required, placeholder = 'Enter text', handleChange }: IProps) => {
   return (
-    <input onChange={(e) => handleChange(e.target.value)} value={value} required placeholder={placeholder} className='w-full break-normal h-12 rounded-md border-1 border-gray-200 px-2 mb-4 bg-gray-100' />
+    <input onChange={(e) => handleChange(e.target.value)} value={value} required={required} placeholder={placeholder} className='w-full break-normal h-12 rounded-md border-1 border-gray-200 px-2 mb-4 bg-gray-100' />
   )
 }
 
