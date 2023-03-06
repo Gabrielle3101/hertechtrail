028f020e5b36314f4c1477f0154d6ab2d2251d6c
Gabrielle3101 <godd3101@gmail.com>
2023-03-06 17:38:28 +0100

    input text

--- src/screens/verify.tsx
+++ src/screens/verify.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Button from '../components/button'
 import { Link } from 'react-router-dom'
 import imageToAdd from "../assets/msg.png";
+import TextInput from '../components/textinput';
 
 const Verify = () => {
   const [value, setValue] = React.useState('');
@@ -10,7 +11,7 @@ const Verify = () => {
       <h3 className=' font-bold text-2xl mb-5'>Verify Your Identity</h3>
       <h5 className=' font-semibold text-1xl mb-3'>Enter the 6-digit OTP sent to 0803xxxxx94</h5>
       <img className='mb-5' src={imageToAdd} alt="Image" />
-      <h5 className=' font-semibold text-green-600 text-1xl mb-3'>__ __ __ __ __ __</h5>
+      <TextInput value={value} handleChange={(e) => setValue(e)} />
       <div className='flex'>
             <h5 className=' font-semibold text-gray-500 text-1xl mb-3'>Didn’t receive an OTP? &nbsp; </h5>
             <a className='text-green-600' href="">Resend</a>
