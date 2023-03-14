cdd77fb9ac61401505c34a9c617a9e1aacdd3f89
Gabrielle3101 <godd3101@gmail.com>
2023-03-14 13:36:06 +0100

    ...

--- src/screens/all.tsx
+++ src/screens/all.tsx
@@ -9,19 +9,19 @@ const All = () => {
       <h3 className=' font-bold text-2xl mb-5'>BB Naija</h3>
       <h5 className=' font-semibold text-1xl mb-3'>Select your favorite candidate</h5>
       <div className="grid grid-cols-2 gap-3">
-            <div className="flex flex-col">
+            <div className="flex flex-col items-center justify-center">
                 <div className='w-32 h-32 rounded-md bg-stone-500 p-4 m-3'></div>
                 <p className="text-gray-500">John Doe</p>
            </div>
-            <div className="flex flex-col">
+            <div className="flex flex-col items-center justify-center">
                 <div className='w-32 h-32 rounded-md bg-stone-500 p-4 m-3'></div>
                 <p className="text-gray-500">John Doe</p>
            </div>
-            <div className="flex flex-col">
+            <div className="flex flex-col items-center justify-center">
                 <div className='w-32 h-32 rounded-md bg-stone-500 p-4 m-3'></div>
                 <p className="text-gray-500">John Doe</p>
            </div>
-            <div className="flex flex-col">
+            <div className="flex flex-col items-center justify-center">
                 <div className='w-32 h-32 rounded-md bg-stone-500 p-4 m-3'></div>
                 <p className="text-gray-500">John Doe</p>
            </div>
--- src/screens/select.tsx
+++ src/screens/select.tsx
@@ -9,7 +9,7 @@ const Select = () => {
     <div className='w-full h-screen flex flex-col  px-6 pt-12'>
       <h3 className=' font-bold text-2xl mb-5'>Select a vote</h3>
       <h5 className=' font-semibold text-1xl mb-3'>Click the button to vote</h5>
-      <div className="grid grid-cols-2 gap-3">
+      <div className="grid grid-cols-2 gap-3 mb-3">
         <div className="border-solid flex flex-col items-center justify-center border border-black rounded-md">
          <div className='w-24 h-fit-content rounded-full bg-green-600 p-4 m-3'></div>
          <p className="text-gray-500">BB Naija</p>
@@ -30,7 +30,7 @@ const Select = () => {
       <Link to={`/all`} className='w-full'>
       <Button2 label="See all" handleClick={() => {}} />
       </Link>
-      <h3 className='text-base text-gray-300 font-bold'>OR</h3>
+      <h3 className='text-base w-full text-gray-300 text-center font-bold'>OR</h3>
       <h5 className=' font-semibold text-1xl mb-3'>Enter vote code</h5>
       <TextInput min='1' max='30' value={value} handleChange={(e) => setValue(e)} placeholder='Enter vote code' />
     </div>
